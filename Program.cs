670998d320adb538300ed113c342113321b9b21e
Berke Akgün <berkeakgun9@gmail.com>
2022-06-17 11:55:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+Console.WriteLine("Welcome to the calculate tax price app");
+
+Console.WriteLine("Enter an integer number : ");
+
+var value1 = Console.ReadLine();
+
+if (string.IsNullOrEmpty(value1))
+{
+    Console.WriteLine("Hata");
+    return;
+}
+if(int.TryParse(value1,out int x))
+{
+
+}
+else
+{
+    Console.WriteLine("Hata");
+    return;
+}
+
+Console.WriteLine("Enter a tax ratio");
+
+var value2 = Console.ReadLine();
+if (string.IsNullOrEmpty(value2))
+{
+    Console.WriteLine("Hata");
+    return;
+}
+if(int.TryParse(value2,out int y))
+{
+
+}
+else
+{
+    Console.WriteLine("Hata");
+    return;
+}
+
+int number = Convert.ToInt32(value1);
+int tax = Convert.ToInt32(value2);
+double tax_price = (number * tax / 100) + number;
+Console.WriteLine("Tax Price is : " + tax_price);
